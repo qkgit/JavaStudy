62af60d22f38e0dc3f8ffbcf891b77daad5c0660
qkgit <qikai1648>
2021-09-06 09:32:10 +0800

    进制

--- 01JavaBase/src/pers/qkgit/javaBase/binary/Binary.java
+++ 01JavaBase/src/pers/qkgit/javaBase/binary/Binary.java
@@ -17,14 +17,16 @@ package pers.qkgit.javaBase.binary;
  */
 public class Binary {
     public static void main(String[] args) {
-        int num1 = 0b110;
-        int num2 = 110;
-        int num3 = 0127;
-        int num4 = 0x110A;
+        int num1 = 0b110;   // 二进制
+        int num2 = 110;     // 十进制
+        int num3 = 0127;    // 八进制
+        int num4 = 0x110A;  // 十六进制
+        int num5 = 0X110A;  // 十六进制
 
-        System.out.println("num1 = " + num1);
-        System.out.println("num2 = " + num2);
-        System.out.println("num3 = " + num3);
-        System.out.println("num4 = " + num4);
+        System.out.println("num1 = " + num1);   // 6
+        System.out.println("num2 = " + num2);   // 110
+        System.out.println("num3 = " + num3);   // 87
+        System.out.println("num4 = " + num4);   // 4362
+        System.out.println("num5 = " + num5);   // 4362
     }
 }
