bbe9267722000a0ba514e00e6aa783b00e9a6460
qkgit <qikai1648>
2021-09-08 11:14:49 +0800

    数组

--- 01JavaBase/src/pers/qkgit/javaBase/array/model/Array.java
+++ 01JavaBase/src/pers/qkgit/javaBase/array/model/Array.java
@@ -29,7 +29,6 @@ package pers.qkgit.javaBase.array.model;
  *  *   ③ 如何获取数组的长度
  *  *   ④ 如何遍历数组
  *  *   ⑤ 数组元素的默认初始化值 ：见ArrayTest1.java
- *  *   ⑥ 数组的内存解析 ：见ArrayTest1.java
  */
 public class Array {
     public static void main(String[] args) {
---
+++ 01JavaBase/src/pers/qkgit/javaBase/array/text/ArrayText1.java
@@ -0,0 +1,58 @@
+package pers.qkgit.javaBase.array.text;
+
+/**
+ * @Auther Qi
+ * @data 2021/9/8 10:36
+ *  * ⑤ 数组元素的默认初始化值
+ *  * 		> 数组元素是整型：0
+ *  * 		> 数组元素是浮点型：0.0
+ *  * 		> 数组元素是char型：0或'\u0000'，而非'0'
+ *  * 		> 数组元素是boolean型：false
+ *  *
+ *  * 		> 数组元素是引用数据类型：null
+ */
+public class ArrayText1 {
+    public static void main(String[] args) {
+        //5.数组元素的默认初始化值
+        System.out.println("*****int*****");
+        int[] arr = new int[4];
+        for(int i = 0;i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("*****short*****");
+        short[] arr1 = new short[4];
+        for(int i = 0;i < arr1.length;i++){
+            System.out.println(arr1[i]);
+        }
+
+        System.out.println("*****float*****");
+        float[] arr2 = new float[5];
+        for(int i = 0;i < arr2.length;i++){
+            System.out.println(arr2[i]);
+        }
+
+        System.out.println("*****char*****");
+        char[] arr3 = new char[4];
+        for(int i = 0;i < arr3.length;i++){
+            System.out.println("----" + arr3[i] + "****");
+        }
+        if(arr3[0] == 0){
+            System.out.println("默认值为0");
+        }
+        if (arr3[0] == '0'){
+            System.out.println("默认值为 '0' ");
+        }
+
+        System.out.println("*****boolean*****");
+        boolean[] arr4 = new boolean[5];
+        System.out.println(arr4[0]);
+
+        System.out.println("*****String（引用数据类型）*****");
+        String[] arr5 = new String[5];
+        System.out.println(arr5[0]);
+        if(arr5[0] == null){
+            System.out.println("北京天气不错！");
+        }
+    }
+}
