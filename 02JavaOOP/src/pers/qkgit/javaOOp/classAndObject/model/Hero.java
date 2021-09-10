e1d1574a43c70030b8ac28206c88d0a052d27718
qkgit <qikai1648>
2021-09-10 16:54:01 +0800

    类与对象

--- 02JavaOOP/src/pers/qkgit/javaOOp/classAndObject/model/Hero.java
+++ 02JavaOOP/src/pers/qkgit/javaOOp/classAndObject/model/Hero.java
@@ -1,5 +1,7 @@
 package pers.qkgit.javaOOp.classAndObject.model;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 /**
  * @Author Qi
  * @data 2021/7/21 17:28
@@ -7,7 +9,8 @@ package pers.qkgit.javaOOp.classAndObject.model;
 //1.创建类，设计类的成员
 public class Hero {
     public String name; //姓名
-    public float hp; //血量
+    public float hp; //当前血量
+    public float maxHP; // 最大生命值
     public float armor; //护甲
     public int moveSpeed; //移动速度
 
@@ -19,13 +22,24 @@ public class Hero {
      *  	   return (返回值);
      *      }
      */
+    // 获取姓名
+    public String getName(){
+        System.out.println("获取姓名"+this.name);
+        return this.name;
+    }
     //获取护甲值
     public float getArmor(){
+        System.out.println("获取护甲");
         return armor;
     }
     //坑队友
     public void keng(){
         System.out.println(name+"坑队友！");
+    }
+    // 击杀
+    public void kill(Hero hero){
+        System.out.println(this.name+"击杀了"+hero.name);
+        recovery();
     }
     // 超神
     public void legendary(){
@@ -33,18 +47,31 @@ public class Hero {
     }
     // 获取当前血量
     public float getHp(){
+        System.out.println(this.hp);
         return this.hp;
     }
-    // 回复
+    // 获取最大生命值
+    public float getMaxHP(){
+        return this.maxHP;
+    }
+    // 生命回复
     public void recovery(float blood){
+        System.out.println("正在回复血量。。。");
         this.hp += blood;
     }
+    // 凯旋回血我不死
+    public void recovery(){
+        this.hp += 0.05 * this.maxHP;
+    }
+
+
 
     @Override
     public String toString() {
         return "Hero{" +
                 "name='" + name + '\'' +
                 ", hp=" + hp +
+                ", maxHP=" + maxHP +
                 ", armor=" + armor +
                 ", moveSpeed=" + moveSpeed +
                 '}';
